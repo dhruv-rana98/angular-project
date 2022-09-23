af4ce86014651f7b10f1ff54de72f9f5e40f9e78
Dhruv Rana <dhruvrana0000@gmail.com>
2022-09-23 11:34:13 +0530

    Added 4 components for 4 pages Login, register, ForgotPassword and Profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ForgotpasswordComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
