b48980277d8bbcf3f00c405a7a5ac0bec2a88012
Dhruv Rana <dhruvrana0000@gmail.com>
2022-09-25 22:18:11 +0530

    Changes for forgot password

--- src/app/forgotpassword/forgotpassword.component.ts
+++ src/app/forgotpassword/forgotpassword.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-forgotpassword',
   templateUrl: './forgotpassword.component.html',
   styleUrls: ['./forgotpassword.component.css']
 })
 export class ForgotpasswordComponent implements OnInit {
-
-  constructor() { }
+  
+  ForgotPasswordForm : FormGroup;
+  
+  constructor() { 
+    this.ForgotPasswordForm = new FormGroup({
+      email : new FormControl('',[ Validators.required, Validators.email]),
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.log(this.ForgotPasswordForm.value.email)
+    if(this.ForgotPasswordForm.valid){
+      this.ForgotPasswordForm.reset();
+    }
   }
 
 }
